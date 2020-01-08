dfedc5fcc16a82aaeaaf651de7f116ad33cb484f
Stanley <AngSanley@users.noreply.github.com>
2020-01-09 00:40:13 +0700

    Updates

---
+++ precache-manifest.26f4b7c53a69415f51df842b89f72ae8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842418fe725c8e77482160177cd0a473",
+    "url": "/binus-react-project/index.html"
+  },
+  {
+    "revision": "f01e744daa3f2e6cd644",
+    "url": "/binus-react-project/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "8969847b4e05b1f879b6",
+    "url": "/binus-react-project/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "f01e744daa3f2e6cd644",
+    "url": "/binus-react-project/static/js/2.556d5624.chunk.js"
+  },
+  {
+    "revision": "8969847b4e05b1f879b6",
+    "url": "/binus-react-project/static/js/main.5c584a71.chunk.js"
+  },
+  {
+    "revision": "1480fe6a11bbadddf115",
+    "url": "/binus-react-project/static/js/runtime~main.493dd871.js"
+  },
+  {
+    "revision": "bbf8c068aa955fd94f7ddc93b12eb394",
+    "url": "/binus-react-project/static/media/cctv.bbf8c068.webm"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/binus-react-project/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "9c0885d900e6fd1fbfcf5b5d2e7ac3a4",
+    "url": "/binus-react-project/static/media/tukar12.9c0885d9.jpg"
+  }
+]);
